3dda64d20024e525789c2367b90be7fc8e0947bf
bigsurmoon <vinny4000@gmail.com>
2022-05-25 09:48:59 +0700

    added testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnRemainder0If1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnRemainder200If800() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnRemainder1000If0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+}
